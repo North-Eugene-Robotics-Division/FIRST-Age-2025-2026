dc0759d67e870ebed108b9b8b0e0f071c5d7e579
Rhone Fleming <151577884+rfleming01@users.noreply.github.com>
2025-10-15 16:42:17 -0700

    Create AutoLeft.java
    Created an autonomous left field file
    Doesn't do anything yet 
    Does build without error

---
+++ MainRobotCode/AutoLeft.java
@@ -0,0 +1,21 @@
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.Hardware;
+
+@Autonomous(name="AutoLeft", group="Linear OpMode")
+public class AutoLeft extends LinearOpMode {
+  
+    @Override
+    public void runOpMode() {
+      
+        waitForStart();
+        // runtime.reset();
+
+        // run until the end of the match (driver presses STOP)
+        while (opModeIsActive()) {
+          
+        }
+    }
+    
+}
