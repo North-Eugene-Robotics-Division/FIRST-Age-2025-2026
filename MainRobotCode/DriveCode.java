e6708562610b713cd43adb3211a7c04b3779dcfc
Rhone Fleming <151577884+rfleming01@users.noreply.github.com>
2025-12-26 12:49:18 -0800

    Update DriveCode.java
    Now uses distance sensors

--- MainRobotCode/DriveCode.java
+++ MainRobotCode/DriveCode.java
@@ -10,69 +10,69 @@ import org.firstinspires.ftc.teamcode.Hardware;
 
 @TeleOp(name="Drive_Code", group="Linear OpMode")
 public class DriveCode extends LinearOpMode {
-    private ElapsedTime runtime = new ElapsedTime();
+	private ElapsedTime runtime = new ElapsedTime();
 
-    @Override
-    public void runOpMode() {
-        
-        Hardware robot = new Hardware(this);
-        robot.init();
-        
-        telemetry.addData("Status", "Initialized");
-        telemetry.update();
-        
-        waitForStart();
-        runtime.reset();
-        
-        
-        // run until the end of the match (driver presses STOP)
-        while (opModeIsActive()) {
-            
-            // POV Mode uses left joystick to go forward & strafe, and right joystick to rotate.
-            double Forward  =  gamepad1.left_stick_y;  // Note: pushing stick forward gives negative value
-            double Rotation =  gamepad1.right_stick_x;
-            double Strafe   =  gamepad1.left_stick_x;
-            robot.driveRobot(Forward, Rotation, Strafe);
-            
-            double LaunchPowerPos = gamepad2.right_trigger;
-            double LaunchPowerNeg = -gamepad2.left_trigger;
-        
-            if (gamepad2.aWasReleased()) {
-                robot.chimneyLaunch();
-            }
-            
-            if (gamepad2.xWasPressed()) {
-                robot.Intake();
-            }
-            
-            if (gamepad1.xWasReleased()) {
-                robot.Normalize();
-            }
-            
-            if (gamepad2.bWasPressed()) {
-                robot.Eject();
-            }
-            
-            if (gamepad1.bWasReleased()) {
-                robot.Normalize();
-            }
-            
-            if (gamepad2.right_trigger > 0) {
-                robot.Launcher(LaunchPowerPos);
-            }
-            
-            
-            if (gamepad2.left_trigger > 0) {
-                robot.Launcher(LaunchPowerNeg);
-            }
-            
-            robot.getDetectedColor();
-            robot.ultrasonic0.update();
-            robot.ultrasonic1.update();
-            robot.ultrasonic2.update();
-            robot.ultrasonic3.update();
+	@Override
+	public void runOpMode() {
+		
+		Hardware robot = new Hardware(this);
+		robot.init();
+		
+		telemetry.addData("Status", "Initialized");
+		telemetry.update();
+		
+		waitForStart();
+		runtime.reset();
+		
+		
+		// run until the end of the match (driver presses STOP)
+		while (opModeIsActive()) {
+			
+			// POV Mode uses left joystick to go forward & strafe, and right joystick to rotate.
+			double Forward  =  gamepad1.left_stick_y;  // Note: pushing stick forward gives negative value
+			double Rotation =  gamepad1.right_stick_x;
+			double Strafe   =  gamepad1.left_stick_x;
+			robot.driveRobot(Forward, Rotation, Strafe);
+			
+			double LaunchPowerPos = gamepad2.right_trigger;
+			double LaunchPowerNeg = -gamepad2.left_trigger;
+		
+			if (gamepad2.aWasReleased()) {
+				robot.chimneyLaunch();
+			}
+			
+			if (gamepad2.xWasPressed()) {
+				robot.Intake();
+			}
+			
+			if (gamepad1.xWasReleased()) {
+				robot.Normalize();
+			}
+			
+			if (gamepad2.bWasPressed()) {
+				robot.Eject();
+			}
+			
+			if (gamepad1.bWasReleased()) {
+				robot.Normalize();
+			}
+			
+			if (gamepad2.right_trigger > 0) {
+				robot.Launcher(LaunchPowerPos);
+			}
+			
+			
+			if (gamepad2.left_trigger > 0) {
+				robot.Launcher(LaunchPowerNeg);
+			}
+			
+			robot.getDetectedColor();
+			robot.ultrasonic0.update();
+			robot.ultrasonic1.update();
+			robot.ultrasonic2.update();
+			robot.ultrasonic3.update();
 
-            robot.telemetryData("DriveCode");
-        }
-    } 
+			robot.telemetryData("DriveCode");
+		}
+	} 
 }
