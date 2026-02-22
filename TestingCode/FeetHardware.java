d37f31a41727b44f8f30f21f8771f0f8afcdd64f
Sternaldesert <90590690+Sternaldesert@users.noreply.github.com>
2026-02-21 16:16:47 -0800

    Refactor FeetHardware to use Foot motor

--- TestingCode/FeetHardware.java
+++ TestingCode/FeetHardware.java
@@ -6,34 +6,34 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 
 public class FeetHardware {
     private LinearOpMode myOpMode = null;
-    public DcMotor motor = null;
+    public DcMotor Foot = null;
     
     public FeetHardware (LinearOpMode opMode) {
         myOpMode = opMode;
     }
     
     public void init(){
-        motor = myOpMode.hardwareMap.get(DcMotor.class, "motor");
+        Foot = myOpMode.hardwareMap.get(DcMotor.class, "motor");
         
         //Sets encoder to 0 at start
-        motor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        Foot.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         
         // Set motor to run without encoders even though we have some
-        motor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODERS);
+        Foot.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODERS);
     }
     
-    int FeetPosition = 3000;
-    boolean isMotorBusy = false;
+    int FeetPosition = 1000;
+    boolean isFootBusy = false;
     
     public void moveFeet(){
-        if (motor.getCurrentPosition() <= FeetPosition) {
-            motor.setPower(1);
-            isMotorBusy = true;
+        if (Foot.getCurrentPosition() <= FeetPosition) {
+            Foot.setPower(1);
+            isFootBusy = true;
         }
         
-        if (motor.getCurrentPosition() > FeetPosition) {
-            motor.setPower(-1);
-            isMotorBusy = true;
+        if (Foot.getCurrentPosition() > FeetPosition) {
+            Foot.setPower(-1);
+            isFootBusy = true;
         }
     }
 }
